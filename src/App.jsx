4b5992d1828923f9abac9ff02d3cf4ba17fadea7
MagicGionson93 <gpasini93@gmail.com>
2025-07-08 10:28:52 +0200

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -5,21 +5,18 @@ import Home from "./pages/Home";
 import { Routes, Route } from "react-router-dom";
 import { MovieProvider } from "./contexts/MovieContext";
 import NavBar from "./components/NavBar";
-import { BrowserRouter } from "react-router-dom";
 
 function App() {
   return (
     <MovieProvider>
-      <BrowserRouter basename="/movieapi">
-        <NavBar />
-        <main className="main-content">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/favorites" element={<Favorites />} />
-            <Route path="/singlemovie/:id" element={<SingleMovie />} />
-          </Routes>
-        </main>
-      </BrowserRouter>
+      <NavBar />
+      <main className="main-content">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/favorites" element={<Favorites />} />
+          <Route path="/singlemovie/:id" element={<SingleMovie />} />
+        </Routes>
+      </main>
     </MovieProvider>
   );
 }
--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/MovieApi/', // IMPORTANTE: il nome del tuo repo GitHub!
+  base: '/movieapi', // IMPORTANTE: il nome del tuo repo GitHub!
   plugins: [react()],
 })
